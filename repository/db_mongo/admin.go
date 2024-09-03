313e8cf546cc1bc1cb7b55f0f3c092f8c6cf45ab
Pande17 <pandedwiwjy@gmail.com>
2024-09-03 16:28:31 +0800

    edit struct admin

--- repository/db_mongo/admin.go
+++ repository/db_mongo/admin.go
@@ -3,8 +3,8 @@ package dbmongo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Admin struct {
-	ID      		primitive.ObjectID 	`bson:"_id,omitempty" json:"id"`
-	AdminName 		string				`bson:"admin_name" json:"admin_name" gorm:"unique"`
-	AdminPassword	string				`bson:"admin_password" json:"admin_password"`
+	ID      		primitive.ObjectID 	`bson:"_id,omitempty" `
+	AdminName 		string				`bson:"admin_name" gorm:"unique"`
+	AdminPassword	string				`bson:"admin_password" `
 	Model
 }
