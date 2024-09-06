c3d64feacaf40d9240def9cf1cb27fdcd09b80bc
Pande17 <pandedwiwjy@gmail.com>
2024-09-06 15:44:58 +0800

    edit base model

--- repository/db_mongo/base.go
+++ repository/db_mongo/base.go
@@ -3,11 +3,10 @@ package dbmongo
 import (
 	"time"
 
-	"gorm.io/gorm"
 )
 
 type Model struct {
 	CreatedAt time.Time			`bson:"created_at"`
 	UpdatedAt time.Time 		`bson:"updated_at"`
-	DeletedAt gorm.DeletedAt	`gorm:"index" bson:"deleted_at"`
+	DeletedAt *time.Time		`bson:"deleted_at,omitempty"`
 }
