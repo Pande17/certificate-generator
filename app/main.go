9a204cd5e72eac86b5e2f0f077add67395e37ac1
Pande17 <pandedwiwjy@gmail.com>
2024-09-06 15:43:52 +0800

    add func Initenv, connectdb, add fiber, call routes

--- app/main.go
+++ app/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"log"
+	"pkl/finalProject/certificate-generator/repository/config"
+	"pkl/finalProject/certificate-generator/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func InitEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	// intitialize environment variables
+	InitEnv()
+
+	// connect to database MongoDB
+	config.ConnectMongoDB()
+
+	// adminCollection := config.GetCollection(client, "certificate-generator", "adminAcc")
+
+	// create a new fiber application instance
+	app := fiber.New()
+
+	// setup API routes
+	routes.RouteSetup(app)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(
+			"Error on running fiber, ", 
+			err.Error())
+	}
+
+
+}
