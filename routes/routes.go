4abe6905da5c77b8d6826b4561c722bcfcecf144
Pande17 <pandedwiwjy@gmail.com>
2024-09-06 15:45:14 +0800

    make routes for api

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,24 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"pkl/finalProject/certificate-generator/controller"
+	"pkl/finalProject/certificate-generator/middleware"
 
-func Routes(r *fiber.App) {
-	// define a route group for organizing the routes
-	certGroup := r.Group("")
+	"github.com/gofiber/fiber/v2"
+)
 
-	// routes for login admin
-	certGroup.Post("/login")	
+func RouteSetup(r *fiber.App) {
+	// define a route group for api routes
+	api := r.Group("")
+
+	api.Post("/signup", controller.SignUp) // routes for signup admin
+	api.Post("/login", controller.Login)	// routes for login admin
+	api.Get("/validate", middleware.RequireAuth, controller.Validate)
+
+
+	// define a route group for template routes
+	// template := r.Group("")
+
+	// template.Get("/") // routes for homepage html
+
 }
