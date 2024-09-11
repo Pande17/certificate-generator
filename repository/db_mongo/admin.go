7142cf6efaf5284f646b52ddc11e6cdda024a112
Pande17 <pandedwiwjy@gmail.com>
2024-09-11 17:46:29 +0800

    add new incremental id for admin account and another comment

--- repository/db_mongo/admin.go
+++ repository/db_mongo/admin.go
@@ -4,7 +4,15 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AdminAccount struct {
 	ID      		primitive.ObjectID 	`bson:"_id,omitempty" `
+	AccID			int64				`bson:"acc_id"`
 	AdminName 		string				`bson:"admin_name"`
 	AdminPassword	string				`bson:"admin_password" `
 	Model
 }
+
+// ADMIN ACCOUNT LIST:
+// usrname : password
+// pande : pande
+// pande2 : pande2
+
+// note: AdminPassword == AdminName
