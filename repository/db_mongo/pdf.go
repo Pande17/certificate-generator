9450f11f96554b7a0f38c36c4c4abf6fc605165f
Lanang-Dhirendra <lanan9.dh1rendra.dwiputra@gmail.com>
2024-09-13 10:15:05 +0800

    add bson tags

--- repository/db_mongo/pdf.go
+++ repository/db_mongo/pdf.go
@@ -4,22 +4,22 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type PDF struct {
 	ID            primitive.ObjectID `bson:"_id,omitempty"`
-	SertifName    string
-	SertifVersion string
-	SertifID      string
+	SertifName    string             `bson:"sertif_name"`
+	SertifVersion string             `bson:"sertif_version"`
+	SertifID      string             `bson:"sertif_id"`
+	NamaPeserta   string             `bson:"nama_peserta"`
+	SKKNI         string             `bson:"skkni"`
+	Kompetensi    string             `bson:"kompetensi"`
+	Validation    string             `bson:"validation"`
+	DataID        string             `bson:"data_id"`
+	TotalJP       uint64             `bson:"total_jp"`
+	Meet          uint64             `bson:"meet"`
+	MeetTime      float64            `bson:"meet_time"`
+	HardSkills    []HardSkill        // ini nggak tau cara implementasinya
+	SoftSkills    []SoftSkill        // ini juga
 	KodeReferal
-	NamaPeserta string
-	SKKNI       string
-	Kompetensi  string
 	ValidDate
-	Validation string
 	KodeQR
-	DataID   string
-	TotalJP  uint64
-	Meet     uint64
-	MeetTime float64
-	HardSkills
-	SoftSkills
 	FinalSkor
 	Model
 }
