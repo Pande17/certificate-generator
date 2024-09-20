8f713f64f2072105b3d8151307f041a4c17b112b
Pande17 <pandedwiwjy@gmail.com>
2024-09-20 11:24:03 +0800

    separate API routes to other functions & add new function to handle routes template

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// function for setup routes
 func RouteSetup(r *fiber.App) {
 	r.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
@@ -14,6 +15,12 @@ func RouteSetup(r *fiber.App) {
 		})
 	})
 
+	APIRoute(r)
+	TemplateRoute(r)
+}
+
+// function for api routes
+func APIRoute(r *fiber.App) {
 	// Define a group routes for API
 	api := r.Group("/api")
 
@@ -30,19 +37,29 @@ func RouteSetup(r *fiber.App) {
 	protected.Post("/logout", controller.Logout) // Route to logout from account
 
 	// Define routes for management admin accounts
-	protected.Get("/accounts", controller.ListAdminAccount)          // Route to see all admin accounts
+	protected.Get("/accounts", controller.GetAllAdminAccount)        // Route to see all admin accounts
 	protected.Get("/accounts/:id", controller.GetAccountByID)        // Route to see admin account detail by acc_id
 	protected.Put("/accounts/:id", controller.EditAdminAccount)      // Route to update password admin account by acc_id
 	protected.Delete("/accounts/:id", controller.DeleteAdminAccount) // Route to delete admin account by acc_id
 
 	// define routes for management competence
-	protected.Post("/competence", controller.CreateKompetensi) // route to create competence data
-	// protected.Put("/competence/:id", controller.EditCompetence) // route to update competence data
-	// protected.Delete("/competence/:id", controller.DeleteCompetence) // route to delete competence data
+	protected.Post("/competence", controller.CreateKompetensi)       // route to create competence data
+	protected.Put("/competence/:id", controller.EditKompetensi)      // route to update competence data
+	protected.Delete("/competence/:id", controller.DeleteKompetensi) // route to delete competence data
 	protected.Get("/competence", controller.GetAllKompetensi)        // route to get all competence data
 	protected.Get("/competence/:id", controller.GetDetailKompetensi) // route to get competence data based on their id
+}
 
-	// define routes for management certificate
-	// protected.Post("/certificate", nil)
-	// protected.Get("/certificate", nil)
+// function for template routes
+func TemplateRoute(r *fiber.App) {
+	accounts := r.Group("/accounts") // define routes group for authentication
+
+	accounts.Get("/login", nil)  // route for template login page
+	accounts.Get("/logout", nil) // route for template logout
+
+	protected := r.Use(middleware.ValidateCookie) // Define protected routes
+
+	protected.Get("/", nil)                   // route for template homepage (display all certificate)
+	protected.Get("/create-certificate", nil) // route for template create certificate
+	protected.Get("/add-competence", nil)     // route for template add competence
 }
