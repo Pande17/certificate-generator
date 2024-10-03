b65efb3737841ad76ade81da7abab253bd6e8254
Lanang-Dhirendra <lanan9.dh1rendra.dwiputra@gmail.com>
2024-10-03 16:02:10 +0800

    rework gen qr api

--- controller/generateQR.go
+++ controller/generateQR.go
@@ -13,24 +13,18 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func GenerateQRCode(str string) error {
-	return qrcode.WriteFile(str, qrcode.Medium, 256, str+".png")
+// LINK STRING NEEDS FIXING (still example only)
+func GenerateQRCode(link, str string) error {
+	return qrcode.WriteFile(link+str, qrcode.Medium, 256, "temp/"+str+".png")
 }
-
-// ini udh bener blum?
 
 // func for create new kompetensi
 func CreateQRCode(c *fiber.Ctx) error {
 	// struct for the incoming request body
 	var qrCodeReq struct {
-		ID        primitive.ObjectID `bson:"_id,omitempty"`
-		QRCodeID  uint64             `bson:"qrcode_id"`
-		QRCodeStr string             `json:"string_qrcode"`
-
-		// !! INGET ISI FIELD LAINNYA !!
-
-		// HardSkills     []dbmongo.HardSkill `json:"hard_skills"`
-		// SoftSkills     []dbmongo.SoftSkill `json:"soft_skills"`
+		ID          primitive.ObjectID `bson:"_id,omitempty"`
+		QRCodeID    uint64             `bson:"qrcode_id"`
+		QRCodePDFID string             `json:"pdf_id"`
 	}
 
 	// parse request body
@@ -38,7 +32,9 @@ func CreateQRCode(c *fiber.Ctx) error {
 		return BadRequest(c, "Failed to read body")
 	}
 
-	if err := GenerateQRCode(qrCodeReq.QRCodeStr); err != nil {
+	link := "https://example.com/"
+
+	if err := GenerateQRCode(link, qrCodeReq.QRCodePDFID); err != nil {
 		return InternalServerError(c, "can't create qr code")
 	}
 
@@ -47,7 +43,7 @@ func CreateQRCode(c *fiber.Ctx) error {
 
 	// check existing qrcode w/ the same str
 	var existingQRCode dbmongo.QRCode
-	filter := bson.M{"string_qrcode": qrCodeReq.QRCodeStr}
+	filter := bson.M{"string_qrcode": qrCodeReq.QRCodePDFID}
 	err := collection.FindOne(context.TODO(), filter).Decode(&existingQRCode)
 	if err == nil {
 		return Conflict(c, "QR Code already exists")
@@ -63,9 +59,10 @@ func CreateQRCode(c *fiber.Ctx) error {
 
 	// struct to input data to db
 	qrCodein := dbmongo.QRCode{
-		ID:        primitive.NewObjectID(),
-		QRCodeID:  uint64(nextQRCodeID),
-		QRCodeStr: qrCodeReq.QRCodeStr,
+		ID:          primitive.NewObjectID(),
+		QRCodeID:    uint64(nextQRCodeID),
+		QRCodePDFID: qrCodeReq.QRCodePDFID,
+		QRCodeLink:  link + qrCodeReq.QRCodePDFID,
 		Model: dbmongo.Model{
 			CreatedAt: time.Now(),
 			UpdatedAt: time.Now(),
