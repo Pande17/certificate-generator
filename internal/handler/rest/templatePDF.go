052f4f7b104f71b8f4010de1182f849fd54a1b11
Lanang-Dhirendra <lanan9.dh1rendra.dwiputra@gmail.com>
2024-10-22 16:08:58 +0800

    dynamic pdf creation for 1st page

--- internal/handler/rest/templatePDF.go
+++ internal/handler/rest/templatePDF.go
@@ -1,7 +1,9 @@
 package rest
 
 import (
+	"log"
 	"os"
+	model "pkl/finalProject/certificate-generator/model"
 
 	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"github.com/gofiber/fiber/v2"
@@ -12,40 +14,57 @@ func TEMPlate(c *fiber.Ctx) error {
 }
 
 func CreatePDF(c *fiber.Ctx) error {
-	var shshsj struct {
-		ID   string `json:"id"`
-		Name string `json:"name"`
+	var dataReq struct {
+		model.CertificateData `json:"data"`
+		Zoom                  float64 `json:"zoom"`
 	}
-	if err := c.BodyParser(&shshsj); err != nil {
+	if err := c.BodyParser(&dataReq); err != nil {
 		return InternalServerError(c, err.Error(), "can't parse body req")
 	}
 
 	pdfg, err := wkhtmltopdf.NewPDFGenerator()
 	if err != nil {
-		return InternalServerError(c, err.Error(), "wkhtmltopdf not found")
+		return InternalServerError(c, "wkhtmltopdf not found", err.Error())
 	}
 
-	f, err := os.Open("temp/index.html")
+	if err := c.Render("temp/index.html", dataReq.CertificateData); err != nil { // remember to change index.html path
+		return InternalServerError(c, "can't render html", err.Error())
+	}
+
+	pdfTempl, err := os.Create("temp/temp" + dataReq.CertificateData.DataID + ".html")
 	if err != nil {
-		return InternalServerError(c, err.Error(), "can't find html file")
+		return InternalServerError(c, "can't create html file", err.Error())
 	}
-	defer f.Close()
+	defer func() {
+		pdfTempl.Close()
+		if err := os.Remove("temp/temp" + dataReq.CertificateData.DataID + ".html"); err != nil {
+			log.Println("WARNING: memory leak (can't remove html file)\nerr:", err)
+		}
+	}()
 
-	pdfg.AddPage(wkhtmltopdf.NewPageReader(f))
+	if _, err := pdfTempl.Write(c.Response().Body()); err != nil {
+		return InternalServerError(c, "can't write to html file", err.Error())
+	}
+
+	page := wkhtmltopdf.NewPage("temp/temp" + dataReq.CertificateData.DataID + ".html")
+	page.Zoom.Set(dataReq.Zoom) // agak tergantung siapa yg buat file pdf-nya, di rendy zoom 1.064, di aku 1.264
+
+	pdfg.AddPage(page)
 	pdfg.MarginTop.Set(0)
 	pdfg.MarginRight.Set(0)
 	pdfg.MarginBottom.Set(0)
 	pdfg.MarginLeft.Set(0)
 	pdfg.Dpi.Set(300)
+	pdfg.PageSize.Set(wkhtmltopdf.PageSizeA4)
 
 	err = pdfg.Create()
 	if err != nil {
 		return InternalServerError(c, err.Error(), "can't create pdf")
 	}
 
-	err = pdfg.WriteFile("temp/" + shshsj.ID + ".pdf")
+	err = pdfg.WriteFile("temp/" + dataReq.DataID + ".pdf")
 	if err != nil {
 		return InternalServerError(c, err.Error(), "can't writefile pdf")
 	}
-	return OK(c, "Success create certificate pdf", shshsj)
+	return OK(c, "Success create certificate pdf", dataReq)
 }
