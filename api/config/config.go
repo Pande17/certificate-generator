fe56dbc2fb92acf7f3494b74e6becb6a14a24bd5
Pande17 <pandedwiwjy@gmail.com>
2024-10-31 11:42:18 +0800

    change from logrus to log

--- api/config/config.go
+++ api/config/config.go
@@ -1,13 +1,14 @@
 package config
 
 import (
+	"log"
+
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
 )
 
 func InitEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		logrus.Warn("Failed loading .env file, using system env")
+		log.Fatal("Failed loading .env file, using system env")
 	}
 }
