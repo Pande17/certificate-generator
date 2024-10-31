fd9dae98b4e960e335a406d622eb231403b8abf7
Pande17 <pandedwiwjy@gmail.com>
2024-10-31 10:16:44 +0800

    merge all certificate's structs

--- api/model/certificateData.go
+++ api/model/certificateData.go
@@ -18,3 +18,29 @@ type CertificateData struct {
 	HardSkills     SkillPDF     `json:"hard_skills" bson:"hard_skills"`
 	SoftSkills     SkillPDF     `json:"soft_skills" bson:"soft_skills"`
 }
+
+type KodeReferral struct {
+	ReferralID int64  `json:"referral_id" bson:"referral_id"`
+	Divisi     string `json:"divisi" bson:"divisi"`
+	BulanRilis string `json:"bulan_rilis" bson:"bulan_rilis"`
+	TahunRilis string `json:"tahun_rilis" bson:"tahun_rilis"`
+}
+
+type ValidDate struct {
+	ValidTotal string `json:"valid_total" bson:"valid_total"`
+	ValidStart string `json:"valid_start" bson:"valid_start"`
+	ValidEnd   string `json:"valid_end" bson:"valid_end"`
+}
+
+type QRCode struct {
+	//QRCodeID    uint64 `json:"qrcode_id" bson:"qrcode_id"`
+	QRCodePDFID string `json:"qrcode_str" bson:"qrcode_str"`
+	QRCodeLink  string `json:"qrcode_link" bson:"qrcode_link"`
+	QRCodeEnc   string `json:"qrcode_enc" bson:"qrcode_enc"`
+}
+
+type SkillPDF struct {
+	Skills          []Skill `json:"skills" bson:"skills"`
+	TotalSkillJP    uint64  `json:"total_skill_jp" bson:"total_skill_jp"`
+	TotalSkillScore float64 `json:"total_skill_score" bson:"total_skill_score"`
+}
