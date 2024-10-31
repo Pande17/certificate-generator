5030e288089b2d3aae36ecfd72f9f46d537afe5f
Pande17 <pandedwiwjy@gmail.com>
2024-10-31 10:17:02 +0800

    delete unused struct files

--- api/model/qrcode.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type QRCode struct {
-	//QRCodeID    uint64 `json:"qrcode_id" bson:"qrcode_id"`
-	QRCodePDFID string `json:"qrcode_str" bson:"qrcode_str"`
-	QRCodeLink  string `json:"qrcode_link" bson:"qrcode_link"`
-	QRCodeEnc   string `json:"qrcode_enc" bson:"qrcode_enc"`
-}
