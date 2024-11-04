782ee3984b07b56a298663c167cce603bdde0e7e
Pande17 <pandedwiwjy@gmail.com>
2024-11-04 12:21:03 +0800

    add function for get Public IP and Get location from IP

--- api/internal/handler/middleware/ip.go
+++ api/internal/handler/middleware/ip.go
@@ -1,31 +1,56 @@
 package middleware
 
-// import (
-// 	"io"
-// 	"net/http"
-// 	"os"
-// )
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
-// // function to get public IP addres from subject
-// func GetPublicIP() (string, error) {
-// 	// make a request to ipify API
-// 	response, err := http.Get("https://api.ipify.org?format=text")
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	defer response.Body.Close()
+// function to get public IP addres from subject
+func GetPublicIP() (string, error) {
+	// make a request to ipify API
+	response, err := http.Get("https://api.ipify.org?format=text")
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
 
-// 	// read the response body
-// 	ip, err := io.ReadAll(response.Body)
-// 	if err != nil {
-// 		return "", err
-// 	}
+	// read the response body
+	ip, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
 
-// 	// return the IP address as a string
-// 	return string(ip), nil
-// }
+	// return the IP address as a string
+	return string(ip), nil
+}
 
-// // function for getting location from IP (implement as per your needs)
-// func GetLocationFromIP(ip string) string {
-// 	apiToken := os.Getenv()
-// }
+// function for getting location from IP (implement as per your needs)
+func GetLocationFromIP(ip string) string {
+	apiToken := os.Getenv("IPINFO")
+	if apiToken == "" {
+		return "API Token not found"
+	}
+
+	url := fmt.Sprintf("https://ipinfo.io/%s/geo?token=%s", ip, apiToken)
+	response, err := http.Get(url)
+	if err != nil {
+		return "Unknown Location"
+	}
+	defer response.Body.Close()
+
+	var result map[string]interface{}
+	if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
+		return "Unknown Location"
+	}
+
+	city, _ := result["city"].(string)
+	country, _ := result["country"].(string)
+
+	if city != "" && country != "" {
+		return fmt.Sprintf("%s, %s", city, country)
+	}
+	return "Unknown Location"
+}
