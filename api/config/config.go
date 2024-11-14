52a7972439e127fa8fb658057b473b61eabcd6e0
IAmAGuest <152069447+Lanang-Dhirendra@users.noreply.github.com>
2024-11-14 12:53:40 +0800

    add temp dir

--- api/config/config.go
+++ api/config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func InitEnv() {
+	log.Println(os.Mkdir("temp", 0644))
 	if err := godotenv.Load(".env"); err != nil {
 		log.Println("Failed loading .env file, using system env")
 	}
