e5f7a2b1c12346cd5dbdc5904b593e898e7162e6
Pande17 <pandedwiwjy@gmail.com>
2024-11-15 14:25:57 +0800

    delete ID field in pdf struct

--- api/model/pdf.go
+++ api/model/pdf.go
@@ -1,13 +1,8 @@
 package model
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type PDF struct {
-	ID         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	DataID     string             `json:"data_id" bson:"data_id"`
-	SertifName string             `json:"sertif_name" bson:"sertif_name"`
-	Data       CertificateData    `json:"data" bson:"data"`
-	Model      `bson:",inline"`   // flatten the model fields
+	DataID     string           `json:"data_id" bson:"data_id"`
+	SertifName string           `json:"sertif_name" bson:"sertif_name"`
+	Data       CertificateData  `json:"data" bson:"data"`
+	Model      `bson:",inline"` // flatten the model fields
 }
