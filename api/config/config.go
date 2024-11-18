6d8f9373dafd89f677ac09d4660de87b0c5b8c76
IAmAGuest <152069447+Lanang-Dhirendra@users.noreply.github.com>
2024-11-18 12:43:31 +0800

    remove dir reading test

--- api/config/config.go
+++ api/config/config.go
@@ -1,11 +1,8 @@
 package config
 
 import (
-	"fmt"
-	"io/fs"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -15,28 +12,5 @@ func InitEnv() {
 	log.Println("make dir assets/certificate err: ", os.Mkdir("assets/certificate", 0644))
 	if err := godotenv.Load(".env"); err != nil {
 		log.Println("Failed loading .env file, using system env")
-	}
-
-	dirEnt, err := os.ReadDir("/")
-	if err != nil {
-		log.Println("readdir error: ", err)
-	} else {
-		readAllDir(dirEnt, 0)
-	}
-}
-
-func readAllDir(dirEnt []fs.DirEntry, indent int) {
-	for _, entry := range dirEnt {
-		fmt.Print(strings.Repeat(" ", indent))
-		fmt.Print(entry.Name())
-		if entry.IsDir() {
-			childEnt, err := os.ReadDir("/")
-			if err != nil {
-				log.Println("readdir error: ", err)
-			} else {
-				readAllDir(childEnt, indent+2)
-			}
-		}
-		fmt.Println()
 	}
 }
