124e7e0b80f5316e3c7903641e8700441976124e
IAmAGuest <152069447+Lanang-Dhirendra@users.noreply.github.com>
2024-11-18 11:25:51 +0800

    simplify AuditMiddleware

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -29,32 +29,32 @@ func RouteSetup(r *fiber.App) {
 	api := r.Group("/api")
 
 	// Define routes for authentication
-	api.Post("/signup", middleware.AuditMiddleware("POST", "account"), rest.SignUp)                           // Route for signing up admin
-	api.Post("/login", middleware.AuditMiddleware("POST", "account"), rest.Login)                             // Route for admin login
-	api.Get("/validate", middleware.ValidateToken, rest.Validate)                                             // Route to check cookie from admin
-	api.Post("/logout", middleware.ValidateToken, middleware.AuditMiddleware("POST", "account"), rest.Logout) // Route to logout from account
+	api.Post("/signup", middleware.AuditMiddleware("account"), rest.SignUp)                           // Route for signing up admin
+	api.Post("/login", middleware.AuditMiddleware("account"), rest.Login)                             // Route for admin login
+	api.Get("/validate", middleware.ValidateToken, rest.Validate)                                     // Route to check cookie from admin
+	api.Post("/logout", middleware.ValidateToken, middleware.AuditMiddleware("account"), rest.Logout) // Route to logout from account
 
 	// Define api routes
 	// Every request to a path with the group "api" always checks the cookie
 	// protected := api.Use(middleware.ValidateCookie)
 
 	// Define routes for management admin accounts
-	api.Get("/accounts", middleware.ValidateToken, middleware.AuditMiddleware("POST", "account"), rest.GetAdminAccount)             // Route to see all admin accounts
-	api.Put("/accounts/:id", middleware.ValidateToken, middleware.AuditMiddleware("POST", "account"), rest.EditAdminAccount)        // Route to update password admin account by acc_id
-	api.Delete("/accounts/:id", middleware.ValidateToken, middleware.AuditMiddleware("DELETE", "account"), rest.DeleteAdminAccount) // Route to delete admin account by acc_id
+	api.Get("/accounts", middleware.ValidateToken, middleware.AuditMiddleware("account"), rest.GetAdminAccount)           // Route to see all admin accounts
+	api.Put("/accounts/:id", middleware.ValidateToken, middleware.AuditMiddleware("account"), rest.EditAdminAccount)      // Route to update password admin account by acc_id
+	api.Delete("/accounts/:id", middleware.ValidateToken, middleware.AuditMiddleware("account"), rest.DeleteAdminAccount) // Route to delete admin account by acc_id
 
 	// define routes for management competence
-	api.Post("/competence", middleware.ValidateToken, middleware.AuditMiddleware("POST", "Competence"), rest.CreateKompetensi)         // route to create competence data
-	api.Get("/competence", middleware.ValidateToken, middleware.AuditMiddleware("GET", "Competence"), rest.GetKompetensi)              // route to get all competence data
-	api.Put("/competence/:id", middleware.ValidateToken, middleware.AuditMiddleware("PUT", "Competence"), rest.EditKompetensi)         // route to update competence data
-	api.Delete("/competence/:id", middleware.ValidateToken, middleware.AuditMiddleware("DELETE", "Competence"), rest.DeleteKompetensi) // route to delete competence data
+	api.Post("/competence", middleware.ValidateToken, middleware.AuditMiddleware("Competence"), rest.CreateKompetensi)       // route to create competence data
+	api.Get("/competence", middleware.ValidateToken, middleware.AuditMiddleware("Competence"), rest.GetKompetensi)           // route to get all competence data
+	api.Put("/competence/:id", middleware.ValidateToken, middleware.AuditMiddleware("Competence"), rest.EditKompetensi)      // route to update competence data
+	api.Delete("/competence/:id", middleware.ValidateToken, middleware.AuditMiddleware("Competence"), rest.DeleteKompetensi) // route to delete competence data
 
 	// define routes for management certificate data
-	api.Post("/certificate", middleware.ValidateToken, middleware.AuditMiddleware("POST", "Certificate"), rest.CreateCertificate)
-	api.Get("/certificate", middleware.ValidateToken, middleware.AuditMiddleware("GET", "Certificate"), rest.GetCertificate)
-	// api.Get("/certificate/:id", middleware.ValidateToken, middleware.AuditMiddleware("GET", "Certificate"), rest.GetCertificateByID)
-	api.Put("/certiticate/:id", middleware.ValidateToken, middleware.AuditMiddleware("PUT", "Certificate"), TEMPlate)
-	api.Delete("/certificate/:id", middleware.ValidateToken, middleware.AuditMiddleware("DELETE", "Certificate"), rest.DeleteCertificate)
+	api.Post("/certificate", middleware.ValidateToken, middleware.AuditMiddleware("Certificate"), rest.CreateCertificate)
+	api.Get("/certificate", middleware.ValidateToken, middleware.AuditMiddleware("Certificate"), rest.GetCertificate)
+	// api.Get("/certificate/:id", middleware.ValidateToken, middleware.AuditMiddleware("Certificate"), rest.GetCertificateByID)
+	api.Put("/certiticate/:id", middleware.ValidateToken, middleware.AuditMiddleware("Certificate"), TEMPlate)
+	api.Delete("/certificate/:id", middleware.ValidateToken, middleware.AuditMiddleware("Certificate"), rest.DeleteCertificate)
 
 	r.Get("/assets/certificate/:id", rest.DownloadCertificate)
 }
