68dffd2e719f5f353a53bfe51e808f508089d6ce
Pande17 <pandedwiwjy@gmail.com>
2024-11-18 15:03:46 +0800

    fix frontend for dockering

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/login/index.jsx"; 
-import Dashboard from "./pages/dashboard/index.jsx";
-import CreatePage from "./pages/create page/index.jsx";
-import AddPage from "./pages/Competence/index.jsx";
-import Layout from "./pages/MainLayout/Layout.jsx"
-import Side from "./pages/Side/index.jsx"
-import Tes from "./pages/tes/Index.jsx"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login/index.jsx';
+import Dashboard from './pages/dashboard/index.jsx';
+import CreatePage from './pages/create page/index.jsx';
+import Competence from './pages/Competence/index.jsx';
+// import AddPage from './pages/Competence/index.jsx';
+import Layout from './pages/MainLayout/Layout.jsx';
+import Side from './pages/Side/index.jsx';
+import Tes from './pages/tes/Index.jsx';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/dashboard" element={<Dashboard />}/>
-      <Route path="/create" element={<CreatePage />}/>
-      <Route path="/competence" element={<AddPage />} />
-      <Route path="/layout" element={<Layout />} />
-      <Route path="/side" element={<Side />} />
-      <Route path="/Tes" element={<Tes />} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<LoginPage />} />
+			<Route path="/dashboard" element={<Dashboard />} />
+			<Route path="/create" element={<CreatePage />} />
+			<Route path="/competence" element={<AddPage />} />
+			<Route path="/layout" element={<Layout />} />
+			<Route path="/side" element={<Side />} />
+			<Route path="/Tes" element={<Tes />} />
+		</Routes>
+	);
 };
 
 export default App;
