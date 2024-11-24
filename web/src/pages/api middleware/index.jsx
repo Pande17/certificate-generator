f937a55dcd8a882cb2e486db70cc4d2f02f9876e
Pande17 <pandedwiwjy@gmail.com>
2024-11-24 15:37:49 +0800

    fix token

--- web/src/pages/api middleware/index.jsx
+++ web/src/pages/api middleware/index.jsx
@@ -1,34 +1,34 @@
-import axios from "axios";
+import axios from 'axios';
 
 // Instance untuk kompetensi
 const Kompetensi = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/competence",
+	baseURL: 'http://127.0.0.1:3000/api/competence',
 });
 
 // Instance untuk sertifikat
 const Sertifikat = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/certificate",
+	baseURL: 'http://127.0.0.1:3000/api/certificate',
 });
 
 // Instance untuk login
 const Login = axios.create({
-  baseURL: "http://127.0.0.1:3000/api/login",
+	baseURL: 'http://127.0.0.1:3000/api/login',
 });
 
 // Interceptor untuk menyisipkan token pada setiap request
 const applyAuthInterceptor = (instance) => {
-  instance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem("token"); // Ambil token dari localStorage
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // Tambahkan Authorization header
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+	instance.interceptors.request.use(
+		(config) => {
+			const authToken = localStorage.getItem('authToken'); // Ambil token dari localStorage
+			if (authToken) {
+				config.headers.Authorization = `Bearer ${authToken}`; // Tambahkan authToken header
+			}
+			return config;
+		},
+		(error) => {
+			return Promise.reject(error);
+		}
+	);
 };
 
 // Terapkan interceptor ke semua instance
