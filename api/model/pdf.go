b0d330a587b6f97c4cefb874ee31f4251f96a8a3
Pande17 <pandedwiwjy@gmail.com>
2024-11-25 14:32:18 +0800

    add adminId field

--- api/model/pdf.go
+++ api/model/pdf.go
@@ -1,8 +1,11 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type PDF struct {
-	DataID     string           `json:"data_id" bson:"data_id"`
-	SertifName string           `json:"sertif_name" bson:"sertif_name"`
-	Data       CertificateData  `json:"data" bson:"data"`
-	Model      `bson:",inline"` // flatten the model fields
+	AdminId    primitive.ObjectID `json:"admin_id" bson:"admin_id"`
+	DataID     string             `json:"data_id" bson:"data_id"`
+	SertifName string             `json:"sertif_name" bson:"sertif_name"`
+	Data       CertificateData    `json:"data" bson:"data"`
+	Model      `bson:",inline"`   // flatten the model fields
 }
--- api/model/signature.go
+++ api/model/signature.go
@@ -1,10 +1,13 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Signature struct {
-	ConfigName string `json:"config_name" bson:"config_name"`
-	Stamp      string `json:"stamp" bson:"stamp"`
-	Signature  string `json:"signature" bson:"signature"`
-	Name       string `json:"name" bson:"name"`
-	Role       string `json:"role" bson:"role"`
+	AdminId    primitive.ObjectID `json:"admin_id" bson:"admin_id"`
+	ConfigName string             `json:"config_name" bson:"config_name"`
+	Stamp      string             `json:"stamp" bson:"stamp"`
+	Signature  string             `json:"signature" bson:"signature"`
+	Name       string             `json:"name" bson:"name"`
+	Role       string             `json:"role" bson:"role"`
 	Model      `bson:",inline"`
 }
