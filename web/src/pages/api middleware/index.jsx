c554e84b263683ddd97dbaec70c656c95622f640
IAmAGuest <152069447+Lanang-Dhirendra@users.noreply.github.com>
2024-11-28 12:23:23 +0800

    readd env, fix env

--- web/src/pages/api middleware/index.jsx
+++ web/src/pages/api middleware/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// Use fallback if REACT_APP_API_URL is undefined
-const api = import.meta.env.REACT_APP_API_URL || "http://localhost:3000";
+// Use fallback if VITE_BACKEND is undefined
+const api = import.meta.env.VITE_BACKEND || "http://localhost:3000";
 
 const Signature = axios.create({
   baseURL: `${api}/api/signature`,
